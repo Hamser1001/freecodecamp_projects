09fe06ad503f197745093ea127ce1b999b1e6470
Hamser1001 <upscale.mind50@gmail.com>
2025-09-11 20:06:36 +0100

    update can access the content

--- probability-calculator-project.py
+++ probability-calculator-project.py
@@ -1,8 +1,23 @@
 import copy
 import random
 
+
 class Hat:
-    pass
+    def __init__(self, **kwargs):
+        self.contents = []
+        for color, count in kwargs.items():
+            self.contents.extend([color] * count)
+
+    def draw(self):
+        pass
+
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
+
+
+hat = Hat(yellow=4, red=2)
+hat2 = Hat(red=5, orange=4, black=1, blue=0, pink=2, yellow=9)
+
+print(hat.contents)
+print(hat2.contents)
