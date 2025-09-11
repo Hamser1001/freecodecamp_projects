fd08d51ae206bac462768f0a0d84b4be0252a427
Hamser1001 <upscale.mind50@gmail.com>
2025-09-11 20:38:15 +0100

    update start experiment function

--- probability-calculator-project.py
+++ probability-calculator-project.py
@@ -8,8 +8,18 @@ class Hat:
         for color, count in kwargs.items():
             self.contents.extend([color] * count)
 
-    def draw(self):
-        pass
+    def draw(self, num):
+        drawn_balls = []
+        if num > len(self.contents):
+            all_balls = self.contents.copy()
+            self.contents = []
+            return all_balls
+        else:
+            for _ in range(num):
+                ball = random.choice(self.contents)
+                self.contents.remove(ball)
+                drawn_balls.append(ball)
+            return drawn_balls
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
@@ -21,3 +31,14 @@ hat2 = Hat(red=5, orange=4, black=1, blue=0, pink=2, yellow=9)
 
 print(hat.contents)
 print(hat2.contents)
+
+
+# print(hat)
+print(f"the results: {hat2.draw(200)}")
+
+probability = experiment(
+    hat=hat,
+    expected_balls={"red": 2, "green": 1},
+    num_balls_drawn=5,
+    num_experiments=2000,
+)
